75f1a6dd26772961e01199141985edbb79668f5c
Jhyuny <wjdgus01227@gmail.com>
2024-05-18 21:10:41 +0900

    transforemr encoder block

--- transformer.py
+++ transformer.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 from .attention import MultiHeadAttention
-from .utils import PositionwiseFeedForward
+from .utils import PositionwiseFeedForward, SublayerConnection
 
 class TransformerBlock(nn.Module):
     """
@@ -10,4 +10,12 @@ class TransformerBlock(nn.Module):
     def __init__(self, hidden, attn_heads, feed_forward_hidden):
         super().__init__()
         self.attnetion = MultiHeadAttention(h=attn_heads, d_model=hidden)
-        self.feed_forward = PositionwiseFeedForward()
+        self.feed_forward = PositionwiseFeedForward(d_model=hidden, d_ff=feed_forward_hidden)
+        self.input_sublayer = SublayerConnection(size=hidden) # Layer Norm
+        self.output_sublayer = SublayerConnection(size=hidden)
+
+    def forward(self, x, mask):
+        x = self.input_sublayer(x, lambda _x:self.attention.forward(_x, _x, _x, mask=mask))
+        x = self.output_sublayer(x, self.feed_forward)
+        return x
+
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class PositionwiseFeedForward(nn.Module):
@@ -15,3 +16,24 @@ class PositionwiseFeedForward(nn.Module):
         # Linear Layer2
         x = self.w_2(x)
         return x
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps # 연산 시 zerodivideerror를 방지
+    
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x-mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module): #Add & Norm
+    def __init__(self, size):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+    
+    def forward(self, x, sublayer):
+        return x + sublayer(self.norm(x))
