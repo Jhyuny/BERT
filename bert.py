47254a17844e62b8b8aa796c1243f0afb6eb761a
jeongjeong2 <wjdgus01227@gmail.com>
2024-05-20 08:11:20 +0900

    BERT Model

--- bert.py
+++ bert.py
@@ -1,18 +1,34 @@
-# Transforemr
+import torch.nn as nn
 
-import torch
-import torch.nn.functional as F
-import math
+from .transformer import TransformerBlock
+from .embeddings import BERTembedding
 
-class Attention(nn.Module):
-    def forward(self, query, key, value, mask = None, dropout = None):
-        scores = torch.matmul(query,key.transpose(-2,-1))/math.sqrt(query.size(-1))
 
-        if mask is not None :
-            scores = scores.masked_fill(mask == 0, -1e9)
-        p_attn = F.softmaxx(scores, dim = 1)
+class BERT(nn.Moduel):
+    def __init__(self, vocab_size, hidden=768, n_layers=12, attn_heads=12, dropout=0.1):
+        super().__init__()
+        self.hidden = hidden
+        self.n_layer = n_layers
+        self.attn_heads = attn_heads
 
-        if dropout is not None :
-            p_attn = dropout(p_attn)
-        
-        return torch.matmul(p_attn, value), p_attn
+        # paper : use 4*hidden_size for ffnetwork hidden size
+        self.feed_forward_hidden = hidden * 4
+
+        # embedding for BERT = token + position + segment
+        self.embedding = BERTembedding(vocab_size=vocab_size, embed_size=hidden)
+
+        # transforemr block
+        self.transforemr_blocks = nn.ModuleList(
+            [TransformerBlock(hidden, attn_heads, hidden * 4) for _ in range(n_layers)]
+        )
+
+    def forward(self, x, segment_info):
+        # attention masking
+        mask = (x > 0).unsqueeze(1).reapeat(1, x.size(1), 1).unsqueeze(1)
+
+        # embedding the indexed sequence to sequence of vectors
+        x = self.embedding(x, segment_info)
+
+        # run multiple transforemr block
+        for transformer in self.transforemr_blocks:
+            x = transformer.forward(x, mask)
--- embeddings.py
+++ embeddings.py
@@ -1,3 +1,54 @@
+import torch.nn as nn
+import torch
+import math
+
+
 # Token Embedding
+class TokenEmbedding(nn.Embedding):
+    def __init__(self, vocab_size, embed_size=512):
+        super().__init__(vocab_size, embed_size, padding_idx=0)
+
+
 # Segment Embedding
-# Position Embedding
+class SegmentEmbedding(nn.Embedding):
+    def __init__(self, embed_size=512):
+        super().__init__(3, embed_size, padding_idx=0)
+
+
+# Positional Embedding
+class PositionalEmbedding(nn.Module):
+    def __init(self, d_model, max_len=512):
+        super().__init__()
+
+        pe = torch.zeros(max_len, d_model).float()
+        pe.required_grad = False  # Positional embedding은 학습되지 않도록 고정
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        # 각 차원의 주기성을 다르게 하기 위해
+        div_term = (
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        ).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)  # 짝수 인덱스 위치
+        pe[:, 1::2] = torch.cos(position * div_term)  # 홀수 인덱스 위치
+
+        # pe tensor를 (1, max_len, d_model)형태로 변환하고 모듈 등록
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        return self.pe[:, : x.size(1)]
+
+
+# BERT Embedding
+class BERTembedding(nn.Module):
+    def __init__(self, vocab_size, embed_size, dropout=0.1):
+        super().__init__()
+        self.token = TokenEmbedding(vocab_size=vocab_size, embed_size=embed_size)
+        self.position = PositionalEmbedding(d_model=self.token.embedding_dim)
+        self.segment = SegmentEmbedding(embed_size=self.token.embedding_dim)
+        self.embed_size = embed_size
+
+    def forward(self, sequence, segment_label):
+        x = self.token(sequence) + self.position(sequence) + self.segment(segment_label)
+        return x
