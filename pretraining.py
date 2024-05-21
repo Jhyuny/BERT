00c1f755797c7c585fb7da2ebb3a1085d307e2ab
Jhyuny <wjdgus01227@gmail.com>
2024-05-21 21:28:51 +0900

    Pretraining

--- pretraining.py
+++ pretraining.py
@@ -1,2 +1,22 @@
 # MLM (Masked Language Model)
+import torch.nn as nn
+
+class MaskedLanguageModel(nn.Module) :
+    def __init__(self, hidden, vocab_size):
+        super().__init__()
+        self.linear = nn.Linear(hidden, vocab_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+    
+    def forward(self, x) :
+        return self.softmax(self.linear(x))
+
 # NSP (Next Sentence Prediction)
+
+class NextSentencePrediction(nn.Module):
+    def __init__(self, hidden) :
+        super().__init__()
+        self.linear = nn.Linear(hidden, 2)
+        self.softmaz = nn.LogSoftmax(dim = -1)
+
+    def forward(self,x):
+        return self.softmax(self.linear(x[:,0]))
