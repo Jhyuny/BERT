d0c1a8e160f60eb5ec21edf5201b147594b6e206
jeongjeong2 <wjdgus01227@gmail.com>
2024-05-24 07:23:14 +0900

    pretraining 주석추가

--- pretraining.py
+++ pretraining.py
@@ -3,34 +3,38 @@ from .bert import BERT
 
 # MLM (Masked Language Model)
 
-class MaskedLanguageModel(nn.Module) :
+
+class MaskedLanguageModel(nn.Module):
     def __init__(self, hidden, vocab_size):
         super().__init__()
         self.linear = nn.Linear(hidden, vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
-    
-    def forward(self, x) :
+
+    def forward(self, x):
         return self.softmax(self.linear(x))
+
 
 # NSP (Next Sentence Prediction)
 
+
 class NextSentencePrediction(nn.Module):
-    def __init__(self, hidden) :
+    def __init__(self, hidden):
         super().__init__()
         self.linear = nn.Linear(hidden, 2)
-        self.softmaz = nn.LogSoftmax(dim = -1)
+        self.softmaz = nn.LogSoftmax(dim=-1)
 
-    def forward(self,x):
-        return self.softmax(self.linear(x[:,0]))
-    
+    def forward(self, x):  # x의 첫번째 요소(문장 벡터)를 선택
+        return self.softmax(self.linear(x[:, 0]))  # softmax 결과로 cls token을 결정
+
+
 # BERT language_model
 class BERTLM(nn.Module):
-    def __init__(self, bert : BERT, vocab_size):
+    def __init__(self, bert: BERT, vocab_size):
         super().__init__()
         self.bert = bert
         self.next_sentence = NextSentencePrediction(self.bert.hidden)
         self.mask_lm = MaskedLanguageModel(self.bert.hidden, vocab_size)
-    
+
     def forward(self, x, segment_label):
         x = self.bert(x, segment_label)
         return self.next_sentence(x), self.mask_lm
